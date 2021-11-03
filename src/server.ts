27cbaf72052f656da960de8b8c4b5a0588435eac
arthurfortunato <luizarthur.fortunato@gmail.com>
2021-11-03 14:33:25 -0300

    Instalando o TypeOrm e o sqlite

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,4 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3000, () => console.log('Server is running'));
+app.listen(3000, () => console.log('Server is running PORT 3000'));
